fa26c3f45783f3e5cdc5bb13d93d352dda4c095f
firdausbiaf <=>
2022-10-31 07:56:30 +0700

    migration data barang

---
+++ database/migrations/2022_10_29_164905_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode', 10)->index();
+            $table->string('nama', 25)->index();
+            $table->string('foto');
+            $table->unsignedBigInteger('harga');
+            $table->unsignedBigInteger('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
